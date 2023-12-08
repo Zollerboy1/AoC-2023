539569245074d3d6aa060aecc48c12b54aa0584f
Josef Zoller <josef@walterzollerpiano.com>
2023-12-08 17:30:16 -0500

    Add Day8

--- Sources/Day8/main.swift
+++ Sources/Day8/main.swift
@@ -6,3 +6,32 @@ let fileContents = try! String(contentsOf: url, encoding: .utf8)
 
 
 let lines = fileContents.split(separator: "\n")
+
+let instructions = lines.first!.map { $0 == "R" }
+
+let maps = Dictionary(uniqueKeysWithValues: lines.dropFirst().map { line in 
+    let regex = /([A-Z0-9]+) = \(([A-Z0-9]+), ([A-Z0-9]+)\)/
+    let match = line.firstMatch(of: regex)!
+    let from = match.1
+    let to = (match.2, match.3)
+    return (from, to)
+})
+
+let starts = Array(maps.keys.filter { $0.last! == "A" })
+
+let counts = starts.map { start in
+    var current = start
+    var count = 0
+    while current.last! != "Z" {
+        let instruction = instructions[count % instructions.count]
+        if instruction {
+            current = maps[current]!.1
+        } else {
+            current = maps[current]!.0
+        }
+        count += 1
+    }
+    return count
+}
+
+print(counts.lcm())
