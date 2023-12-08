e708ef0cebb62c4999cba1c9fb8d8c4b77ab9b88
Josef Zoller <josef@walterzollerpiano.com>
2023-12-08 17:28:12 -0500

    Add Day6

--- Sources/Day6/main.swift
+++ Sources/Day6/main.swift
@@ -6,3 +6,32 @@ let fileContents = try! String(contentsOf: url, encoding: .utf8)
 
 
 let lines = fileContents.split(separator: "\n")
+let splitLines = lines.map { $0.drop { $0 != ":" }.dropFirst().split(separator: " ").map { Int($0)! } }
+
+let races = zip(splitLines[0], splitLines[1])
+
+let result = races.map { time, distance in
+    var count = 0
+    for holdTime in 0...time {
+        if (time - holdTime) * holdTime > distance {
+            count += 1
+        }
+    }
+    return count
+}.reduce(1, *)
+
+let race = lines.map { String($0.drop { $0 != ":" }.dropFirst().filter { $0 != " " }) }.prefix(2).map { Int($0)! }
+
+print(race[0])
+var count = 0
+for holdTime in 0...race[0] {
+    if holdTime % 1000000 == 0 {
+        print(holdTime)
+    }
+    if (race[0] - holdTime) * holdTime > race[1] {
+        count += 1
+    }
+}
+
+print(result)
+print(count)
